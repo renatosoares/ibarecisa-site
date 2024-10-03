ef01f71ef88524f24042bae63f92f28c95917ea0
Renato Soares <renatosoares@rits.com.br>
2024-10-03 17:25:29 -0300

    build: remove dist folder

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker compose exec node sh -c "npm run build"
 
 cd dist
 
-echo ibarecisa.renatosoares.dev >CNAME
+echo ibarecisa.renatosoares.dev > CNAME
 
 git init -b main
 git add -A
@@ -15,3 +15,5 @@ git commit -m 'deploy'
 git push -f git@github.com:renatosoares/ibarecisa-site.git main:gh-pages
 
 cd -
+
+rm -rf dist
