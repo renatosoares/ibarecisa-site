c09b77af51d9951989291b5fa65690795ef58e9a
Renato Soares <renatosoares@rits.com.br>
2024-11-13 11:29:00 -0300

    feat: add liks to sections

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,15 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+
+const bsNavbarCollapse = new bootstrap.Collapse('#navbarCollapse', {
+  toggle: false
+});
+
+document.querySelectorAll(".nav-link")
+  .forEach((navLinkElement) => {
+    navLinkElement.addEventListener("click", () => {
+      bsNavbarCollapse.hide();
+    });
+  })
