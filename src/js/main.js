7eec46a34c40edab16280b172d4024b1a573b334
Renato Soares <renatosoarespro@hotmail.com>
2025-07-05 08:06:28 -0300

    feat: list the latest broadcasts

--- src/js/main.js
+++ src/js/main.js
@@ -42,3 +42,60 @@ import * as bootstrap from 'bootstrap'
     lastScrollY = currentScrollY;
   };
 })();
+
+(() => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("last-broadcasts-solemn-worship");
+
+    fetch("solemn-worship.json")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Erro ao carregar o JSON");
+        }
+        return response.json();
+      })
+      .then(data => {
+        data
+          .slice(-3)
+          .reverse()
+          .forEach(item => {
+            const col = document.createElement("div");
+            col.className = "col";
+
+            col.innerHTML = `
+              <div class="card shadow-sm">
+                <iframe
+                  class="bd-placeholder-img card-img-top"
+                  height="225"
+                  width="100%"
+                  preserveAspectRatio="xMidYMid slice"
+                  role="img"
+                  src="${item.videoUrl}"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerpolicy="strict-origin-when-cross-origin"
+                  allowfullscreen
+                ></iframe>
+
+                <div class="card-body">
+                  <p class="card-text">${item.description}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-body-secondary">${item.timestamp}</small>
+                  </div>
+                </div>
+              </div>
+            `;
+
+            container.appendChild(col);
+          });
+      })
+      .catch(error => {
+        console.error("Erro ao carregar os dados:", error);
+      });
+  });
+})();
