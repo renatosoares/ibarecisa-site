41372ba76ec3dbb78661dba62bfc319294c5e578
Renato Soares <renatosoarespro@hotmail.com>
2025-07-26 06:52:24 -0300

    chore: updated with latest broadcasts

--- src/js/main.js
+++ src/js/main.js
@@ -82,8 +82,7 @@ import * as bootstrap from 'bootstrap'
                   <p class="card-text">${item.description}</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+
                     </div>
                     <small class="text-body-secondary">${item.timestamp}</small>
                   </div>
