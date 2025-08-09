1ccd1000e559985de7d46db9d1833804a51936a4
Renato Soares <renatosoarespro@hotmail.com>
2025-08-09 06:01:55 -0300

    chore: switch to main branch and set author

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git checkout main
+
 docker compose exec node sh -c "npm run build"
 
 cd dist
@@ -9,6 +11,7 @@ cd dist
 echo ibarecisa.org.br > CNAME
 
 git init -b main
+git config user.email "renatosoarespro@hotmail.com"
 git add -A
 git commit -m 'deploy'
 
